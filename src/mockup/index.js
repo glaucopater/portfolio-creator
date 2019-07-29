4ecebd2f85b08fdfe0865c4dfc9801049fda30e1
Glauco Pater <>
2019-07-29 09:37:39 +0200

    Enable Dark Mode. Add new Galleries

--- src/mockup/index.js
+++ src/mockup/index.js
@@ -20,5 +20,20 @@ export const data = {
       published_at: '2015-05-27T21:22:26.670000+00:00',
       url: 'portrait',
     },
+    {
+      gallery: 'Nature',
+      published_at: '2015-05-27T21:22:26.670000+00:00',
+      url: 'nature',
+    },
+    {
+      gallery: 'Still Life',
+      published_at: '2015-05-27T21:22:26.670000+00:00',
+      url: 'stilllife',
+    },
+    {
+      gallery: 'Abstract',
+      published_at: '2015-05-27T21:22:26.670000+00:00',
+      url: 'abstract',
+    },
   ],
 };
