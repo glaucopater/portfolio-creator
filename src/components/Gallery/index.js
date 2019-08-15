46dfef54f27987e84eb7c8a7f4d7f5e561434462
Glauco Pater <>
2019-08-16 00:00:52 +0200

    Add navigation event to the gallery, keeping the existing one

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { StyledGallery } from './styled';
 import Image from '../Image/';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Route } from 'react-router-dom';
 
 export default class Gallery extends PureComponent {
   static propTypes = {
@@ -12,18 +12,22 @@ export default class Gallery extends PureComponent {
 
   render() {
     const { gallery, url } = this.props;
-
+    const galleryUrl = `/gallery/${url}`;
     return (
-      <StyledGallery>
-        <figure>
-          <Image src={url} alt={gallery} />
-          <figcaption>
-            <h3>
-              <NavLink to={`/gallery/${url}`}>{gallery}</NavLink>
-            </h3>
-          </figcaption>
-        </figure>
-      </StyledGallery>
+      <Route
+        render={({ history }) => (
+          <StyledGallery onClick={() => history.push(galleryUrl)}>
+            <figure>
+              <Image src={url} alt={gallery} />
+              <figcaption>
+                <h3>
+                  <NavLink to={galleryUrl}>{gallery}</NavLink>
+                </h3>
+              </figcaption>
+            </figure>
+          </StyledGallery>
+        )}
+      />
     );
   }
 }
