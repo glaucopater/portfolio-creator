80c127cf9b683facbea38f725e7efba9bc1f4f76
Glauco Pater <>
2019-10-13 16:41:07 +0200

    Code refactoring

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,3 +4,4 @@ export const IMAGE_DETAILS_ENDPOINT = '/api/galleries/';
 export const DEFAULT_MARGIN = '1rem';
 export const DEFAULT_PADDING = '1rem 0';
 export const OFFLINE = true;
+export const FILE_EXTENSION = '.jpg';
