56729fe3df9ae210524fb99c2d7adca659a58493
Glauco Pater <>
2020-02-10 23:52:07 +0100

    Update dependencies

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,5 +3,6 @@
 import requestAnimationFrame from './tempPolyfills';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import 'mutationobserver-shim';
 
 configure({ adapter: new Adapter() });
